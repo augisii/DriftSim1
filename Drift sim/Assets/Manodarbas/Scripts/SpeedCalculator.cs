99b1420abf5cde1c5d3ed724754a1525c499a0c3
augisii <146208481+augisii@users.noreply.github.com>
2024-01-07 14:11:51 +0200

    First
    .

---
+++ Drift sim/Assets/Manodarbas/Scripts/SpeedCalculator.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SpeedCalculator : MonoBehaviour
+{
+    public float Speed;
+    public Rigidbody rb;
+
+    public Text SpeedText;
+    public Text GearText;
+    public Text RPMText;
+
+    // Conversion factor from meters per second to kilometers per hour
+    private const float MpsToKph = 3.6f;
+
+    void FixedUpdate()
+    {
+        Vector3 vel = rb.velocity;
+        Speed = rb.velocity.magnitude * MpsToKph;
+
+        SpeedText.text = Speed.ToString("0");
+    }
+}
