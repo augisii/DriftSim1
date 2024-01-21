99747c143778ee8198d86307b1e4784b44fe1fa8
augisii <146208481+augisii@users.noreply.github.com>
2024-01-21 13:55:33 +0200

    Almost
    .

--- Manodriftsimul/Assets/MODELIAI/Scripts/DriftScoreManager.cs
+++ Manodriftsimul/Assets/MODELIAI/Scripts/DriftScoreManager.cs
@@ -1,14 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class DriftScoreManager : MonoBehaviour
 {
-    public static int currentScore = 0;
-    public static int scoreMultiplier = 1;
+    public WheelController wheelController;
+    public TextMeshProUGUI driftScoreText;
+    public TextMeshProUGUI multiplierText;
 
-    void Start()
+    private float driftScore;
+    private float driftMultiplier;
+    private bool isDrifting;
+    public float initialDriftMultiplier = 1.0f;
+    public float driftMultiplierIncreaseRate = 0.1f;
+
+    // Update is called once per frame
+    void Update()
     {
-        // Optionally, you can initialize variables or perform setup here.
+        CalculateDriftScore();
+        UpdateDriftScoreText();
+        UpdateMultiplierText();
     }
 
-    // You can add more methods or functionality as needed.
+    void CalculateDriftScore()
+    {
+        float driftFactor = Mathf.Abs(wheelController.Horizontal);
+
+        if (driftFactor > 0.1f) // If drifting to the side
+        {
+            isDrifting = true;
+
+            // Add your drift score calculation logic here
+            // For example, you can use the wheel's rotation speed or other parameters
+            // In this example, a simple calculation is used for demonstration purposes.
+            driftScore += driftFactor * Time.deltaTime * (initialDriftMultiplier + driftMultiplier);
+
+            // Increase the drift multiplier immediately when drifting
+            driftMultiplier += driftMultiplierIncreaseRate * Time.deltaTime;
+        }
+        else // If not drifting
+        {
+            if (isDrifting)
+            {
+                // Reset the drift multiplier when drifting ends
+                driftMultiplier = 0;
+                isDrifting = false;
+            }
+        }
+    }
+
+    void UpdateDriftScoreText()
+    {
+        driftScoreText.text = "Drift Score:\n" + Mathf.Round(driftScore);
+    }
+
+    void UpdateMultiplierText()
+    {
+        multiplierText.text = "Multiplier:\n" + Mathf.Round(driftMultiplier * 10) / 10f;
+    }
 }
--- Manodriftsimul/Assets/MODELIAI/Scripts/WheelController.cs
+++ Manodriftsimul/Assets/MODELIAI/Scripts/WheelController.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 
 public class WheelController : MonoBehaviour
 {
-
     public WheelAlignment[] steerableWheels;
 
     public float BreakPower;
@@ -19,34 +18,45 @@ public class WheelController : MonoBehaviour
     public float wheelAcceleration;
     public float wheelMaxSpeed;
 
-
-
     public Rigidbody RB;
 
     // Update is called once per frame
     void Update()
     {
+        GetInput();
         wheelControl();
     }
 
+    // Get player input
+    void GetInput()
+    {
+        Horizontal = 0f;
 
-    //Applies steering and motor torque
+        if (Input.GetKey(KeyCode.A))
+        {
+            Horizontal = -1f;
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            Horizontal = 1f;
+        }
+
+        Vertical = Input.GetAxis("Vertical");
+    }
+
+    // Applies steering and motor torque
     void wheelControl()
     {
         for (int i = 0; i < steerableWheels.Length; i++)
         {
-            //Sets default steering angle
+            // Sets default steering angle
             steerableWheels[i].steeringAngle = Mathf.LerpAngle(steerableWheels[i].steeringAngle, 0, Time.deltaTime * wheelRotateSpeed);
-            //Sets default motor speed
+
+            // Sets default motor speed
             steerableWheels[i].wheelCol.motorTorque = -Mathf.Lerp(steerableWheels[i].wheelCol.motorTorque, 0, Time.deltaTime * wheelAcceleration);
 
-
-
-            //Motor controls
-
-            Horizontal = Input.GetAxis("Horizontal");
-            Vertical = Input.GetAxis("Vertical");
-
+            // Motor controls
             if (Vertical > 0.1)
             {
                 steerableWheels[i].wheelCol.motorTorque = -Mathf.Lerp(steerableWheels[i].wheelCol.motorTorque, wheelMaxSpeed, Time.deltaTime * wheelAcceleration);
@@ -62,17 +72,10 @@ public class WheelController : MonoBehaviour
                 RB.drag = 0;
             }
 
-
-            if (Horizontal > 0.1)
+            if (Horizontal > 0.1 || Horizontal < -0.1)
             {
-                steerableWheels[i].steeringAngle = Mathf.LerpAngle(steerableWheels[i].steeringAngle, -wheelSteeringAngle, Time.deltaTime * wheelRotateSpeed);
-            }
-
-            if (Horizontal < -0.1)
-            {
-                steerableWheels[i].steeringAngle = Mathf.LerpAngle(steerableWheels[i].steeringAngle, wheelSteeringAngle, Time.deltaTime * wheelRotateSpeed);
+                steerableWheels[i].steeringAngle = Mathf.LerpAngle(steerableWheels[i].steeringAngle, Horizontal * wheelSteeringAngle, Time.deltaTime * wheelRotateSpeed);
             }
         }
     }
-
 }
